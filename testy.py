5d3a5edf5a88493871831a38e3aae7485fa32c84
Jakub Liput <jakub.liput@gmail.com>
2012-11-03 22:56:23 +0100

    Dodany test konwersji wideo (wymagany plik wejściowy input.flv); Poprawki w innych testach

--- testy.py
+++ testy.py
@@ -6,8 +6,13 @@
 
 import os
 import subprocess
+import time
+
+null = open('/dev/null', 'w')
 
 def t_calloc_many():
+	global null
+	
 	print 'test alokacji pamieci'
 	results = []
 	
@@ -22,9 +27,11 @@ def t_calloc_many():
 	
 	#print results
 	
-	print reduce(lambda x, y: x + y, results) / len(results)
+	print '%.2f' % (reduce(lambda x, y: x + y, results) / len(results))
 
 def t_copy():
+	global null
+	
 	print 'test kopiowania pliku'
 	
 	block_size = 1000*1000
@@ -34,40 +41,63 @@ def t_copy():
 	filename2 = 'zero2.bin'
 	
 	subprocess.check_call(['dd', 'if=/dev/zero', 'of=%s' % filename1,
-		'bs=%d' % block_size, 'count=%d' % block_count], stderr=subprocess.STDOUT)
+		'bs=%d' % block_size, 'count=%d' % block_count], stdout=null, stderr=null)
 	
-	#out = subprocess.check_output(['echo', 'Jozek'])
-	
-	out = subprocess.check_output(['/usr/bin/time', '-f', '%e',
-		'cp', filename1, filename2], stderr=subprocess.STDOUT)
+	#out = subprocess.check_output(['/usr/bin/time', '-f', '%e',
+		#'cp', filename1, filename2], stderr=subprocess.STDOUT)
+
+	start = time.time()
+	subprocess.check_call(['cp', filename1, filename2], stdout=null, stderr=null)
+	end = time.time()
 	
 	os.remove(filename1)
 	os.remove(filename2)
 	
-	print float(out)
+	#print float(out)
+	print '%.2f' % (end-start)
 
+def t_video():
+	global null
 	
+	print 'test konwersji wideo'
+	
+	#null = open("NUL","w")
+	
+	input_file = 'input.flv'
+	output_file = 'output.mpg'
+	
+	start = time.time()
+	subprocess.check_call(['ffmpeg', '-y', '-i', input_file, output_file], stdout=null, stderr=null)
+	end = time.time()
+	
+	#null.close()
+	
+	os.remove(output_file)
+		
+	print '%.2f' % (end-start)
 
 def main():
 	try:
 		subprocess.check_call(['make', 'all'])
 	except OSError as e:
 		print 'problem z budowaniem: %s' % (str(e))
-		exit(1)
+		return 1
 	
 	# TODO mapa i do argumentów
-	targets = [t_calloc_many, t_copy]
-	#targets = [t_copy]
+	targets = [t_calloc_many, t_copy, t_video]
+	#targets = [t_video]
 	
 	try:
 		for t in targets:
 			t()
 	except OSError as e:
 		print 'problem z testem %s: %s' % (str(t), str(e))
-		exit(2)
+		return 2
 		
 	pass
 
 if __name__ == '__main__':
-	main()
+	e = main()
+	null.close()
+	exit(e)
 	
