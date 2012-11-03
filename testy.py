4d287f32998814dc50177f567db6179b03c94b77
Jakub Liput <jakub.liput@gmail.com>
2012-11-03 20:42:05 +0100

    Test kopiowania pliku

--- testy.py
+++ testy.py
@@ -12,37 +12,60 @@ def t_calloc_many():
 	results = []
 	
 	block = 1024*1024*256 # 256MB
-	blocks_count = 100000#0
-	tests_count = 10
+	blocks_count = 100000
+	tests_count = 3
 	
 	
 	for i in range(tests_count):
-		p = subprocess.Popen(['./test_mem', 'calloc_many', str(block), str(blocks_count)], 
-			stdout=subprocess.PIPE)
-		out, err = p.communicate()
+		out = subprocess.check_output(['./test_mem', 'calloc_many', str(block), str(blocks_count)])
 		results.append(float(out))
 	
 	#print results
 	
 	print reduce(lambda x, y: x + y, results) / len(results)
+
+def t_copy():
+	print 'test kopiowania pliku'
 	
-	return err
+	block_size = 1000*1000
+	block_count = 512
 	
+	filename1 = 'zero.bin'
+	filename2 = 'zero2.bin'
+	
+	subprocess.check_call(['dd', 'if=/dev/zero', 'of=%s' % filename1,
+		'bs=%d' % block_size, 'count=%d' % block_count], stderr=subprocess.STDOUT)
+	
+	#out = subprocess.check_output(['echo', 'Jozek'])
+	
+	out = subprocess.check_output(['/usr/bin/time', '-f', '%e',
+		'cp', filename1, filename2], stderr=subprocess.STDOUT)
+	
+	os.remove(filename1)
+	os.remove(filename2)
+	
+	print float(out)
+
+	
+
 def main():
-	if os.system('make all') != 0:
-		print 'problem z budowaniem'
+	try:
+		subprocess.check_call(['make', 'all'])
+	except OSError as e:
+		print 'problem z budowaniem: %s' % (str(e))
 		exit(1)
 	
-	targets = [t_calloc_many]
+	# TODO mapa i do argumentów
+	targets = [t_calloc_many, t_copy]
+	#targets = [t_copy]
 	
-	for t in targets:
-		t()
-		# TODO
-		#if t() != 0:
-			#print 'problem z testem:', t
-			#break
-	
-	
+	try:
+		for t in targets:
+			t()
+	except OSError as e:
+		print 'problem z testem %s: %s' % (str(t), str(e))
+		exit(2)
+		
 	pass
 
 if __name__ == '__main__':
